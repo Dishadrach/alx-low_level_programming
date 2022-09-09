ae2645ef8ecc1e23d076c1a523797b73fb058243
Dishadrach <adamsshadrachdickson@gmail.com>
2022-09-09 03:14:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,14 +9,3 @@ int main(void)
 	        return (0);
 }
 
-5) #include <stdio.h>
-/**
- *  * main - main block
- *   * Return: 0
- *    */
-int main(void)
-{
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
-}
-
