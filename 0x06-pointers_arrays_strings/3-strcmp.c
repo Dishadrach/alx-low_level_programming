171b8e6c9d1c7e37a338c9c02dc680f0368ae16d
Dishadrach <adamsshadrachdickson@gmail.com>
2022-09-22 14:49:01 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,27 @@ int _strcmp(char *s1, char *s2)
 	int diff = 0;
 	int i = 0;
 
-	while (s1[i])
+	while (1)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			diff = ((s1[i] - '\0') - (s2[i] - '\0'));
+			diff = (s2[i]);
 			break;
 		}
-		i++;
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-		return (diff);
+	return (diff);
 }
