e2f85e634be9ec274ba05d500fe113b229e8ed5a
Dishadrach <adamsshadrachdickson@gmail.com>
2022-09-23 02:04:47 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,31 @@
 #include "main.h"
 
 /**
- * leet - leet encoder
- * @str: string to be encoded
- *
- * Return: address of the encoded string
+ * leet - Encodes a string into 1337
+ * @c: String of variable pointer
+ * Return: String that is encoded
  */
 
-char *leet(char *)
+char *leet(char *c)
 {
-	int i = 0;
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
 
-	while (str[i] != '\0')
+	while (*c)
 	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char ×)
-{
-	char mapping_low[8] = {'0', '1', '\0', 'e', 'a', '\0', '\0', 't'}
-	char mapping_upper[8] = {'0', '1', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 0)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i]);
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			replacement = i + '0';
-			break;
+			/*32 is the difference between lower case letters and apper case letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
 		}
-		i++;
+		c++;
 	}
-	return (replacement);
+
+	return (cp);
+
 }
