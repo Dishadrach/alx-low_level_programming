884f16ca8c4453cc106f7c56d99a8b3229230ce8
Dishadrach <adamsshadrachdickson@gmail.com>
2022-10-21 01:39:02 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+#include "strlen.c"
 
 list_t *createNewNode(const char *str);
 
@@ -38,31 +39,3 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (*head);
 }
-
-/**
- * createNewNode - create a new list_t list node
- * @str: pointer to string to be assigned to the created node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
- */
-
-list_t *createNewNode(const char *str)
-{
-	list_t *new_node_ptr = malloc(sizeof(list_t));
-
-	if (!new_node_ptr)
-		return (NULL);
-
-	new_node_ptr->str = strdup(str);
-
-	if (!(new_node_ptr->str))
-	{
-		free(new_node_ptr);
-		return (NULL);
-	}
-	
-	new_node_ptr->len = _strLen(new_node_ptr->str);
-	new_node_ptr->next = NULL;
-
-	return (new_node_ptr);
-}	
