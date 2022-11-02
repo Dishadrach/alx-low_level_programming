0107c41a0e031f3ebbf9806c383c51cb906b80d0
Dishadrach <adamsshadrachdickson@gmail.com>
2022-11-02 01:54:35 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,26 +1,18 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <elf.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-int append_text_to_file(const char *filename, char *text_content);
-int create_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned
-void validate_elf(unsigned char *e_ident);
-unsigned int lit_to_big_endian(unsigned int x);
-void print_entry(unsigned int e_type, unsigned char *e_ident);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
